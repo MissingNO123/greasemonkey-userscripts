e6ed28358314e2c8fb0c1d0b889b6d7ee395b04b
Julia <MissingNO123@users.noreply.github.com>
2022-02-19 19:42:48 -0500

    Create YouTube-UnShortify.user.js

---
+++ YouTube-UnShortify.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name     	Youtube Un-Shortify
+// @description Redirect YouTube #Shorts to regular video pages
+// @author	 	MissingNO123
+// @namespace https://alteregobot.me
+// @version  	1
+// @grant    	none
+// @match    	https://*.youtube.com/shorts/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+		window.location.href = (window.location.href).replace(/\.com\/shorts\//, ".com/watch?v=");
+})();
