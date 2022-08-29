6e3835d517c2db0ebb34b2428861e4876733a484
MissingNO123 <MissingNO123@users.noreply.github.com>
2022-08-29 16:01:22 -0400

    Change const to var
    Apparently GM doesn't like es6

--- BoothAutoLanguageChanger.user.js
+++ BoothAutoLanguageChanger.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     		Booth Automatic Language Changer
-// @description	Change from en/ja/ko/zh/etc to your preferred language
+// @description	Change from ja/ko/zh/etc to your preferred language
 // @version  		1
 // @namespace		https://greasyfork.org/en/users/878446-missingno123
 // @grant    		none
@@ -8,11 +8,11 @@
 // @run-at      document-start
 // ==/UserScript==
 
-const prefLocale = '/en/'; // en, ja, ko, zh-tw, zh-cn
-const reger = /(\/[a-zA-Z]{2}\/)|(\/[a-zA-Z]{2}-[a-zA-Z]{2}\/)/i;
+var prefLocale = '/en/'; // en, ja, ko, zh-tw, zh-cn
+var reger = /(\/[a-zA-Z]{2}\/)|(\/[a-zA-Z]{2}-[a-zA-Z]{2}\/)/i;
 
-const addr = window.location.toString();
-const locale = addr.match(reger)[0];
+var addr = window.location.toString();
+var locale = addr.match(reger)[0];
 
 if (locale !== prefLocale) {
 	window.location.replace(addr.replace(reger, prefLocale));
